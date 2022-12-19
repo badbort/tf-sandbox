4cb23a38f1f831f4a20d12afb218bcd166c42f21
Alek Ivankovic <alek.ivankovic@gmail.com>
2022-12-19 15:15:18 +0800

    Dummy 7 + fmt fix

--- test/main.tf
+++ test/main.tf
@@ -3,15 +3,16 @@
 resource "null_resource" "codacy_project_token" {
 
   provisioner "local-exec" {
-    interpreter   = ["pwsh", "-Command"]
-    when          = create
-          command = <<EOF
+    interpreter = ["pwsh", "-Command"]
+    when        = create
+    command     = <<EOF
 
 Write-Output "Hello World. Lets go to sleep"
 Get-Date
 Start-Sleep -Seconds 120
 Get-Date
 Write-Output "Fin"
+
 
       EOF
   }

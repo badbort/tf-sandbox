11825a14c55799dfbb3d41548a031c5d5c102edc
Alek Ivankovic <alek.ivankovic@gmail.com>
2022-12-19 09:34:50 +0800

    Create main.tf

---
+++ test/main.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "codacy_project_token" {
+
+  provisioner "local-exec" {
+    when        = create
+          command = <<EOF
+
+Write-Output "Hello World. Lets go to sleep"
+Get-Date
+Start-Sleep -Seconds 120
+Get-Date
+Write-Output "Fin"
+
+      EOF
+    interpreter = ["pwsh", "-Command"]
+  }
+}
